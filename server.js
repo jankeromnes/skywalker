12eb7583b0a60dca5e51bd6d4c900b33b6a1a040
Jan Keromnes <janx@linux.com>
2012-01-24 23:23:00 +0100

    [Server] Start parsing Solar System PCK file
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- server.js
+++ server.js
@@ -2,9 +2,25 @@
  * Copyright © 2011 Jan Keromnes, Thaddee Tyl. All rights reserved.
  * Code covered by the LGPL license. */
 
-var camp = require('./camp/camp').start({
-  port: 80,
-  secure: false,
-  debug: 10
+
+// Import the modules we need
+var spice = require('./lib/spice.js'),   // NASA data tools
+    camp = require('./camp/camp.js');    // WebApp Server
+
+
+// The data we will be working with
+var Space = {};
+
+
+// Load the Solar System PCK file we got from NASA's SPICE data
+spice.readPCK('../data/pck00010.tpc', function(PCK) {
+  Space['SolarSystem'] = PCK;
 });
 
+
+// Start the server with options from argv (e.g. `sudo node server.js 80 no 0`)
+camp.start({
+  port: +process.argv[2],
+  secure: process.argv[3] === 'yes',
+  debug: +process.argv[4]
+});
